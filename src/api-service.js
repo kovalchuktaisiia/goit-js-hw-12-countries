b9383bfa54f29e260fcf12f65d31517dca74dad1
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-07-21 15:10:50 +0300

    new

---
+++ src/api-service.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountry(CountryName) {
+  return fetch(`${BASE_URL}/name/{name}`).then(response =>
+    response.json(),
+  );
+}
+
+export default { fetchCountry };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './css/styles.css';
+import API from './api-service';
 
 const DEBOUNCE_DELAY = 300;
+
+
+
